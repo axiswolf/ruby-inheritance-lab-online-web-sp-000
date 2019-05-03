e5916a76f7e4bccab0f2a00a18f5c3d11205fe32
Karen V <karenvo@usc.edu>
2019-05-03 08:42:54 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,4 @@
 class User
-  attr_accessor :first_name, :last_name, :knowledge
-  attr_reader :first_name, :last_name, :knowledge
-  def initialize(knowledge)
-    @knowledge = []
-  end
+  attr_accessor :first_name, :last_name
+  attr_reader :first_name, :last_name
 end

d37af29897172a9e9ec60772858e96588be13138
Karen V <karenvo@usc.edu>
2019-05-03 08:42:19 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,4 @@
 class User
-  attr_accessor :first_name, :last_name
-  attr_reader :first_name, :last_name
-
-
+  attr_accessor :first_name, :last_name, :knowledge
+  attr_reader :first_name, :last_name, :knowledge
 end

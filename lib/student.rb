c7f2eb0a31dbfb1319898b7cb01f33fc318863c1
Karen V <karenvo@usc.edu>
2019-05-03 08:45:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
 class Student < User
-  def initialize(knowledge)
+  def initialize
+    @knowledge = []
+  end
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+  def knowledge
+    @knowledge
+  end
 end
